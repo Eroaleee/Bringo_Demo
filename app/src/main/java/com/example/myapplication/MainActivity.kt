c223d67b5176f4b321d7f2e29f0b3e917901bb0e
2amazing4me <razvanmihai1337@yahoo.com>
2023-10-16 11:13:49 +0300

    Fixed crash on startup (wrong typecast of ImageButton to Button) + QOL changes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.myapplication
 
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -20,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         rvAddressList.adapter = addressAdapter
         rvAddressList.layoutManager = LinearLayoutManager(this)
 
-        findViewById<Button>(R.id.btnAddAddress).setOnClickListener {
+        findViewById<ImageButton>(R.id.btnAddAddress).setOnClickListener {
             val addressName = findViewById<EditText>(R.id.etAddressName).text.toString()
             if(addressName.isNotEmpty()) {
                 val address = Address(addressName)
