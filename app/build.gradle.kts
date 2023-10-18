639677d4d1c4dec5eab062b418161f099d06cc0f
2amazing4me <razvanmihai1337@yahoo.com>
2023-10-18 20:22:09 +0300

    Added fastest route algorithm to the start button (currently running on a 4s delay instead of ASAP (will need to find a way to improve))

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,9 +57,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0") // For JSON conversion
     implementation("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    implementation("androidx.compose.runtime:runtime:1.5.3")
 }
--- app/src/main/java/com/example/myapplication/JSONHandler.kt
+++ app/src/main/java/com/example/myapplication/JSONHandler.kt
@@ -2,7 +2,6 @@ package com.example.myapplication
 
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
-import kotlinx.serialization.json.Json
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
